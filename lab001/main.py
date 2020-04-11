d5cb889e48dae77146adf16a26b71333aded67df
ermolkin <uyermolkin@hubspot.com>
2020-04-11 13:48:53 +0300

    [lab001] Update plotting of image samples

--- lab001/main.py
+++ lab001/main.py
@@ -2,32 +2,44 @@ import cv2
 import os
 from matplotlib import pyplot as plt
 import random
+import pdb
 
 # 1: load data and show some images
 
+# Returns data in format { 'folders': {}, 'images': { 'name': file, ...} }
 def load_images_from_folder(folder):
-    images = []
+    print("Getting images from {}".format(folder))
+    result = { 'folders': {}, 'images': {} }
     for name in os.listdir(folder):
         is_folder = os.path.isdir(os.path.join(folder, name))
         if is_folder:
             subfolder = os.path.join(folder, name)
-            images += load_images_from_folder(subfolder)
+            result['folders'][name] = load_images_from_folder(subfolder)
         else:
             img = cv2.imread(os.path.join(folder, name))
             if img is not None:
-                images.append(img)
-    return images
+                result['images'][name] = img
+    return result
 
-def show_image(image):
-    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-    plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
+def plot_samples(images, sample_size, name):
+    figure = plt.figure()
+    figure.suptitle(name)
+    folders = images['folders']
+    for folder_name, folder in folders.items():
+        image_name_samples = random.sample(list(folder['images']), sample_size)
+        for image_name in image_name_samples:
+            subplot = figure.add_subplot(
+                sample_size,
+                len(folders),
+                list(folders).index(folder_name) * sample_size + image_name_samples.index(image_name) + 1
+            )
+            subplot.imshow(folder['images'][image_name])
+            subplot.set_axis_off()
     plt.show()
 
-img_dir_small = './notMNIST_small'
-img_dir_large = './notMNIST_large'
+test_sample_folder = './notMNIST_small'
+train_sample_folder = './notMNIST_large'
 
-images_small = load_images_from_folder(img_dir_small)
-images_large = load_images_from_folder(img_dir_large)
-
-for image in random.choices(images_small, k=10):
-    show_image(image)
+train_sample = load_images_from_folder(train_sample_folder)
+test_sample = load_images_from_folder(test_sample_folder)
+plot_samples(test_sample, 10, 'hello')
